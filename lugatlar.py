a30e044f65742cb39cc2d179b5180e8c4114e4eb
Odiljon Atabaev <odiljonatabaev@gmail.com>
2023-05-29 15:57:13 +0500

    Create lugatlar.py
    lugatlar fayli qo'shildi

---
+++ lugatlar.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 15:41:41 2023
+
+@author: Odiljon
+"""
+
+car_0 = {'model':'ferrari', 'rang':'qizil'}
+# =============================================================================
+# print(car_0['model'])
+# print(car_0['rang'])
+# 
+# =============================================================================
+# =============================================================================
+# en_uz = {'apple':'olma', 'apricot':'o\'rik', 'banana':'banan'}
+# print(en_uz['apple'])
+# 
+
+# =============================================================================
+# talaba_0 = {'ism':'murod olimov', 'yosh':20, 't_yil':2000}
+# print(f"{talaba_0['ism'].title()} \
+# {talaba_0['t_yil']}-yilda tug\'ilgan, \
+# {talaba_0['yosh']} yoshda")
+# talaba_0['kurs']=4
+# talaba_0['fakultet']='informatika'
+# talaba_0['ism']='abdulloh'
+# 
+# =============================================================================
+talaba_1 = {}
+talaba_1['ism']='qobil rasulov'
+talaba_1['kurs']=3
+talaba_1['yosh'] = 20
+print(talaba_1)
